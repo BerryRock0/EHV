990374f82bac6aa2d093f0e0200114fc48e23c71
BerryRock0 <mdenials128@gmail.com>
2026-04-25 07:21:49 -0400

    Update EtherMain.java

--- src/main/java/EtherHack/Ether/EtherMain.java
+++ src/main/java/EtherHack/Ether/EtherMain.java
@@ -1,31 +1,33 @@
 package EtherHack.Ether;
 
+import EtherHack.Ether.EtherAPI;
+import EtherHack.Ether.EtherCredits;
+import EtherHack.Ether.EtherLuaManager;
+import EtherHack.Ether.EtherTranslator;
+import EtherHack.utils.Logger;
+
 public class EtherMain {
-
     private static EtherMain instance;
-    public EtherGUI etherGUI;
+    public EtherTranslator etherTranslator;
+    public EtherCredits etherCredits;
+    public EtherLuaManager etherLuaManager;
     public EtherAPI etherAPI;
-    public EtherInitializer etherInitializer;
 
-    /**
-     * Предотвращение создание экземпляров из вне
-     */
     private EtherMain() {
     }
 
-    /**
-     * Реализация инициализации EtherHack
-     */
-    public void initEther(){
-        etherInitializer = new EtherInitializer();
-        etherAPI = new EtherAPI();
-        etherGUI = new EtherGUI();
+    public void init() {
+        Logger.printLog((String)"Initializing EtherHack...");
+        this.etherTranslator = new EtherTranslator();
+        this.etherTranslator.loadTranslations();
+        this.etherCredits = new EtherCredits();
+        this.etherAPI = new EtherAPI();
+        this.etherAPI.loadAPI();
+        this.etherLuaManager = new EtherLuaManager();
+        this.etherLuaManager.loadLua();
+        Logger.printLog((String)"Initialization EtherHack was completed!");
     }
 
-    /**
-     * Получение текущего экземпляра синглтона EtherHack
-     * @return Экземпляр EtherMain
-     */
     public static EtherMain getInstance() {
         if (instance == null) {
             instance = new EtherMain();
