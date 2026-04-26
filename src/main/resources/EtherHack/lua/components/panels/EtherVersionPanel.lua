0088bcd0634244fb77f7b3ece08c5d966fca33a2
BerryRock0 <mdenials128@gmail.com>
2026-04-26 11:36:45 -0400

    Update EtherVersionPanel.lua

--- src/main/resources/EtherHack/lua/components/panels/EtherVersionPanel.lua
+++ src/main/resources/EtherHack/lua/components/panels/EtherVersionPanel.lua
@@ -1,1 +1,86 @@
+require "ISUI/ISPanel"
 
+--*********************************************************
+--* Глобальные установки UI
+--*********************************************************
+EtherVisualsPanel = ISPanel:derive("EtherVisualsPanel"); -- Наследование от ISPanel
+
+--*********************************************************
+--* Обработка prerender
+--*********************************************************
+function EtherVisualsPanel:prerender()
+    self:setStencilRect(0,10,self:getWidth(),self:getHeight() - 20);
+    ISPanel.prerender(self);
+end
+
+--*********************************************************
+--* Обработка render
+--*********************************************************
+function EtherVisualsPanel:render()
+    ISPanel.render(self);
+    self:clearStencilRect();
+end
+
+--*********************************************************
+--* Обработка событий колесика мыши
+--*********************************************************
+function EtherVisualsPanel:onMouseWheel(del)
+	self:setYScroll(self:getYScroll() - (del * 40));
+	return true;
+end
+
+
+function EtherVisualsPanel:createChildren()
+    ISPanel.createChildren(self);
+
+    self:setScrollChildren(true);
+    self:setScrollHeight(0);
+    self:addScrollBars();
+
+    self.entry = ISTextEntryBox:new(""), 10, self.configs.y + self.configs.height + 10, self.width / 2 - 60, 24);
+    self.entry.font = UIFont.Small;
+    self.entry:initialise();
+    self.entry:instantiate();
+    self:addChild(self.entry);
+
+    local changeButton = UIButton:new(self.entry.x + self.entry.width + 10, self.entry.y, 80, 24, getTranslate("UI_Settings_ConfigSave"), function ()
+        local versionString = self.entry:getText();
+        if (configName ~= "") then
+            changeVersion(versionString);
+        end
+    end)
+    changeButton:initialise();
+    changeButton:instantiate();
+    changeButton:setAnchorLeft(true);
+    changeButton:setAnchorRight(false);
+    changeButton:setAnchorTop(false);
+    changeButton:setAnchorBottom(true);
+    changeButton.update = function ()
+        local text = self.entry:getText();
+        if (text ~= "") then
+            changeButton.isEnable = true;
+        else
+            changeButton.isEnable = false;
+        end
+    end
+    self:addChild(changeButton)
+end
+--*********************************************************
+--* Создание нового экземпляра меню
+--*********************************************************
+function EtherVisualsPanel:new(posX, posY, width, height)
+    local menuTableData = {};
+
+    menuTableData = ISPanel:new(posX, posY, width, height);
+    setmetatable(menuTableData, self);
+    menuTableData.background = true;
+	menuTableData.backgroundColor = {r=0.0, g=0.0, b=0.0, a=0.0};
+	menuTableData.borderColor = {r=0.0, g=0.0, b=0.0, a=0.0};
+    menuTableData.moveWithMouse = true;
+    menuTableData.yRowPosition = 10;
+    self.__index = self;
+
+    self.uiElements = {}; -- Список всех элементов
+
+    return menuTableData;
+end
