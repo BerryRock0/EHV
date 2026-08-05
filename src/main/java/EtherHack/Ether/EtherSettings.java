96f704f1ebcdce2a76d0ae21d89bd0a0d9ff174d
BerryRock0 <mdenials128@gmail.com>
2026-08-05 11:54:30 +0000

    Update EtherSettings.java

--- src/main/java/EtherHack/Ether/EtherSettings.java
+++ src/main/java/EtherHack/Ether/EtherSettings.java
@@ -11,22 +11,4 @@ public class EtherSettings
     public void set(EtherValue ev, boolean value) {
         value[ev.ordinal()] = value;
     }
-
-    public void saveConfig(String configFileName)
-    {
-        Properties config = new Properties();
-        FileOutputStream out = new FileOutputStream("EtherHack/config/" + configFileName + ".properties");
-        
-        for (EtherValue ev : EtherValue.values())
-            config.setProperty(ev.name(), ev.is(ev));
-
-        try {config.store(out, null);}
-        catch (Exception e) {return;}
-    }
-    public void loadConfig()
-    {
-
-
-        
-    }
 }
